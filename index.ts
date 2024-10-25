d7b00ea6c2f9e2f127c68386cf5f9eac0f9ba794
Matthew Kay <matthewkay@live.com>
2024-10-25 01:43:58 +0100

    Add image processing

--- index.ts
+++ index.ts
@@ -1,31 +1,38 @@
-import { S3CreateEvent, S3Handler } from "aws-lambda";
-import AWS from 'aws-sdk';
+import { APIGatewayProxyEvent, APIGatewayProxyResultV2, Handler } from "aws-lambda";
+import { removeBackground } from "@imgly/background-removal-node";
 
-const s3 = new AWS.S3();
-
-export const handler: S3Handler = async (event: S3CreateEvent): Promise<void> =>  {
-    console.log('Received S3 event:', JSON.stringify(event, null, 2));
-
-    for (const record of event.Records) {
-      const bucketName = record.s3.bucket.name;
-      const objectKey = record.s3.object.key;
-  
-      try {
-        const s3Object = await s3.getObject({
-          Bucket: bucketName,
-          Key: objectKey,
-        }).promise();
-  
-        console.log(`Successfully retrieved object: ${objectKey} from bucket: ${bucketName}`);
-  
-        // Process object here (e.g., log its content, process its data, etc.)
-        const objectData = s3Object.Body?.toString('utf-8');
-        console.log(`Object content: ${objectData}`);  
-      }
-      catch (error) {
-        console.error(`Error retrieving or processing object ${objectKey} from bucket ${bucketName}`, error);
-        throw error;
-      }
+export const handler: Handler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResultV2> =>  {
+  try {
+    if (!event.body || !event.isBase64Encoded) {
+      return {
+        statusCode: 400,
+        body: JSON.stringify({ message: "No image found in the request" }),
+      };
     }
+
+    const imageBuffer = Buffer.from(event.body, "base64");
+
+    const blob = await removeBackground(imageBuffer);
+    const buffer = Buffer.from(await blob.arrayBuffer());
+
+    const base64ProcessedImage = `data:image/png;base64,${buffer.toString("base64")}`;
+
+    return {
+      statusCode: 200,
+      headers: {
+        "Content-Type": "image/png",
+        "Content-Disposition": "inline",
+      },
+      body: base64ProcessedImage,
+      isBase64Encoded: true,
+    };
+  }
+  catch (error) {
+    console.error("Error processing image:", error);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ message: "Internal server error" }),
+    };
+  }
 };
   
